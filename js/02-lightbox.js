53628f722cb11170d3f4e732d5128f45b6891ffb
Tetiana-UA <izidaxm85@gmail.com>
2023-11-17 16:52:02 +0100

    5

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,13 +9,10 @@ function createMarkup(arr) {
     .map(
       ({ preview, original, description }) => `
     <li class="gallery__item">
-    <a class="gallery__link" href="${original} ">
-    <img
-    class="gallery__image"
-    src="${preview}"
-    alt="${description}"/>
-    </a>
-    </li>
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
     `
     )
     .join("");
